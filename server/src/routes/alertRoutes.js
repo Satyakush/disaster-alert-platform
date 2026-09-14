d420f458f2f165eb432af5a6c32ed928e004ad70
Satyam Kushwaha <satykush123456789@gmail.com>
2026-09-14 21:33:01 +0530

    feat: add alert lifecycle status route

--- server/src/routes/alertRoutes.js
+++ server/src/routes/alertRoutes.js
@@ -1,20 +1,20 @@
 import express from "express";
-import { createAlert, getAllAlerts, updateAlert,deleteAlert,} from "../controllers/alertController.js";
+import {
+  createAlert,
+  getAllAlerts,
+  updateAlert,
+  updateAlertStatus,
+  deleteAlert,
+} from "../controllers/alertController.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import adminMiddleware from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getAllAlerts);
-
-// Admin-only: Create alert
 router.post("/", authMiddleware, adminMiddleware, createAlert);
-
-// Update alert (Admin)
- router.put("/:id", authMiddleware, adminMiddleware, updateAlert);
-
-// Delete alert (Admin)
+router.put("/:id", authMiddleware, adminMiddleware, updateAlert);
+router.patch("/:id/status", authMiddleware, adminMiddleware, updateAlertStatus);
 router.delete("/:id", authMiddleware, adminMiddleware, deleteAlert);
 
 export default router;
-
