2bd05cdd6a8417cfea87cbd8a5b2ffc0ed4e869b
Satyam Kushwaha <satykush123456789@gmail.com>
2026-09-14 23:08:43 +0530

    feat: expose evacuation route endpoint

--- server/src/routes/evacuationRoutes.js
+++ server/src/routes/evacuationRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { getEvacuationGuidance, getNearestShelters } from "../controllers/evacuationController.js";
+import { getEvacuationRoute } from "../controllers/evacuationRouteController.js";
 
 const router = express.Router();
 
 router.get("/nearest-shelters", getNearestShelters);
 router.get("/guidance", getEvacuationGuidance);
+router.get("/route", getEvacuationRoute);
 
 export default router;
