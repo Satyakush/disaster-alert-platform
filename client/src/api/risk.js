8fae97572d805916beb1e8a382f3dfd2816ae22e
Satyam Kushwaha <satykush123456789@gmail.com>
2026-09-14 23:50:01 +0530

    Fix risk analysis request payload

--- client/src/api/risk.js
+++ client/src/api/risk.js
@@ -1,6 +1,6 @@
 import api from "./axios";
 
-export const analyzeRisk = async (region) => {
-  const res = await api.post("/risk/analyze", region);
+export const analyzeRisk = async (region, hazard = {}) => {
+  const res = await api.post("/risk/analyze", { region, hazard });
   return res.data;
 };
