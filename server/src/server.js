53c73b45c821b2c07ff233f3cbffc467b45df828
Satyam Kushwaha <satykush123456789@gmail.com>
2026-09-15 14:37:17 +0530

    Mount disaster analytics API

--- server/src/server.js
+++ server/src/server.js
@@ -13,6 +13,7 @@ import shelterRoutes from "./routes/shelterRoutes.js";
 import evacuationRoutes from "./routes/evacuationRoutes.js";
 import responseTaskRoutes from "./routes/responseTaskRoutes.js";
 import resourceRoutes from "./routes/resourceRoutes.js";
+import analyticsRoutes from "./routes/analyticsRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 const clientOrigins = (process.env.CLIENT_URL || "http://localhost:5173")
@@ -51,6 +52,7 @@ app.use("/api/shelters", shelterRoutes);
 app.use("/api/evacuation", evacuationRoutes);
 app.use("/api/response-tasks", responseTaskRoutes);
 app.use("/api/resources", resourceRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
